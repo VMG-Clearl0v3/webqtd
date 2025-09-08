646187e0562a25e3b27b1c20748e6506b7def5d6
VMG-Clearl0v3 <ph.vtru0ng.1610@gmail.com>
2025-09-08 17:06:03 +0700

    sua loi timeout

--- src/app/component/DetailProductHeader.tsx
+++ src/app/component/DetailProductHeader.tsx
@@ -19,7 +19,7 @@ export default function DetailProductHeader({title, image}: DetailProductHeaderP
     />
   </div>
 
-  <div className="relative z-10 max-w-5xl mx-auto flex flex-col md:flex-row items-center justify-between px-6 py-10 mt-20">
+  <div className="relative z-10 max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between px-6 py-10 mt-20">
     <div className="w-full md:w-[350px] aspect-[4/3] md:h-[250px] relative rounded-lg overflow-hidden shadow-lg order-1 md:order-2 mb-4 md:mb-0">
       <Image
         src={image || "/image/noimage.jpg"}
@@ -32,7 +32,7 @@ export default function DetailProductHeader({title, image}: DetailProductHeaderP
     {/* Chữ */}
     <h1
       className="
-        text-3xl font-bold text-[#00377B] drop-shadow-lg max-w-lg
+       text-3xl font-bold text-[#00377B] drop-shadow-lg max-w-lg
         text-center md:text-left order-2 md:order-1
       "
     >
--- src/app/san-pham/cho-vay/[slug]/page.tsx
+++ src/app/san-pham/cho-vay/[slug]/page.tsx
@@ -19,7 +19,7 @@ export default async function LoanProductDetailPage({ params }: { params: Promis
         <h2 className="text-3xl md:text-4xl text-center mb-10 font-bold text-[#00377B] tracking-wide">Chi tiết sản phẩm</h2>
         <div className="text-[#00377B] space-y-8">
         {/* Điều kiện vay vốn */}
-        <div className="bg-blue-50 border border-gray-200 rounded-2xl p-6 shadow-sm">
+        <div className="bg-blue-50 rounded-2xl p-6 shadow-sm">
         <div className="flex items-center gap-2 pb-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -32,9 +32,9 @@ export default async function LoanProductDetailPage({ params }: { params: Promis
           <path d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625Z" />
           <path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
         </svg>
-        <h1 className="text-2xl font-semibold">Điều kiện vay vốn</h1>
+        <h1 className="text-xl font-semibold">Điều kiện vay vốn</h1>
         </div>
-        <ul className="text-lg list-disc pl-6 space-y-2">
+        <ul className="md:text-lg sm:text-md list-disc pl-6 space-y-2 text-justify">
           {conditionItems.map((item, idx) => (
           <li key={idx}>{item}</li>
           ))}
@@ -42,7 +42,7 @@ export default async function LoanProductDetailPage({ params }: { params: Promis
         </div>
 
         {/* Tính năng */}
-        <div className="bg-blue-50 border border-gray-200 rounded-2xl p-6 shadow-sm">
+        <div className="bg-blue-50 rounded-2xl p-6 shadow-sm">
         <div className="flex items-center gap-2 pb-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -55,16 +55,16 @@ export default async function LoanProductDetailPage({ params }: { params: Promis
           <path d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625Z" />
           <path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
         </svg>
-        <h1 className="text-2xl font-semibold">Tính năng</h1>
+        <h1 className="text-xl font-semibold">Tính năng</h1>
         </div>
-        <ul className="text-lg list-disc pl-6 space-y-2">
+        <ul className="md:text-lg sm:text-md list-disc pl-6 space-y-2 text-justify">
           {featureItems.map((item, idx) => (
           <li key={idx}>{item}</li>
           ))}
         </ul>
         </div>
            {/* Hồ sơ thủ tục*/}
-        <div className="bg-blue-50 border border-gray-200 rounded-2xl p-6 shadow-sm">
+        <div className="bg-blue-50 rounded-2xl p-6 shadow-sm">
         <div className="flex items-center gap-2 pb-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -77,9 +77,9 @@ export default async function LoanProductDetailPage({ params }: { params: Promis
           <path d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625Z" />
           <path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
         </svg>
-        <h1 className="text-2xl font-semibold">Hồ sơ thủ tục</h1>
+        <h1 className="text-xl font-semibold">Hồ sơ thủ tục</h1>
         </div>
-        <ul className="text-lg list-disc pl-6 space-y-2">
+        <ul className="md:text-lg sm:text-md list-disc pl-6 space-y-2 text-justify">
           {documentItems.map((item, idx) => (
           <li key={idx}>{item}</li>
           ))}
