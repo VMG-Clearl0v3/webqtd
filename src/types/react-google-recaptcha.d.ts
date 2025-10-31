6e291d80dd82f6978788572b3db33128c0e435f9
VMG-Clearl0v3 <ph.vtru0ng.1610@gmail.com>
2025-10-31 09:48:23 +0700

    add contact page

---
+++ src/types/react-google-recaptcha.d.ts
@@ -0,0 +1,18 @@
+declare module "react-google-recaptcha" {
+  import * as React from "react";
+
+  interface ReCAPTCHAProps {
+    sitekey: string;
+    onChange?: (token: string | null) => void;
+    onExpired?: () => void;
+    onErrored?: () => void;
+    theme?: "light" | "dark";
+    size?: "compact" | "normal" | "invisible";
+    tabindex?: number;
+  }
+
+  export default class ReCAPTCHA extends React.Component<ReCAPTCHAProps> {
+    reset(): void;
+    execute(): void;
+  }
+}
